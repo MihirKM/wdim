a08886ed32256916a5a3920d391c4815bafcbca0
Mihir Mukherjee <mihir.m@digipen.edu>
2021-12-02 16:06:21 -0800

    Made the Teleporter model work properly

--- wdim/src/main/java/com/mihir/wdim/block/custom/TeleporterBlock.java
+++ wdim/src/main/java/com/mihir/wdim/block/custom/TeleporterBlock.java
@@ -1,5 +1,7 @@
 package com.mihir.wdim.block.custom;
 
+import java.util.stream.Stream;
+
 import com.mihir.wdim.world.dimension.ModDimensions;
 import com.mihir.wdim.world.dimension.SimpleTeleporter;
 
@@ -11,6 +13,11 @@ import net.minecraft.util.ActionResultType;
 import net.minecraft.util.Hand;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.BlockRayTraceResult;
+import net.minecraft.util.math.shapes.IBooleanFunction;
+import net.minecraft.util.math.shapes.ISelectionContext;
+import net.minecraft.util.math.shapes.VoxelShape;
+import net.minecraft.util.math.shapes.VoxelShapes;
+import net.minecraft.world.IBlockReader;
 import net.minecraft.world.World;
 import net.minecraft.world.server.ServerWorld;
 
@@ -19,6 +26,17 @@ public class TeleporterBlock extends Block {
 		super(properties);
 	}
 
+	private static final VoxelShape SHAPE = Stream.of(
+			Block.makeCuboidShape(5, 2, 5, 11, 8, 11),
+			Block.makeCuboidShape(5, 8, 5, 11, 14, 11),
+			Block.makeCuboidShape(0, 0, 0, 16, 2, 16),
+			Block.makeCuboidShape(0, 14, 0, 16, 16, 16)
+			).reduce((v1, v2) -> VoxelShapes.combineAndSimplify(v1, v2, IBooleanFunction.OR)).get();
+	@Override
+	public VoxelShape getShape(BlockState state, IBlockReader worldIn, BlockPos pos, ISelectionContext context) {
+		return SHAPE;
+	}
+	
 	@SuppressWarnings("deprecation")
 	@Override
 	public ActionResultType onBlockActivated(BlockState state, World worldIn, BlockPos pos,
