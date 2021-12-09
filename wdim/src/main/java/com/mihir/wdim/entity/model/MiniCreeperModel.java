4c99352bf2a777718e5c8966a91056568d87908b
Mihir Mukherjee <mihir.m@digipen.edu>
2021-12-08 16:44:41 -0800

    Mostly working model

--- wdim/src/main/java/com/mihir/wdim/entity/model/MiniCreeperModel.java
+++ wdim/src/main/java/com/mihir/wdim/entity/model/MiniCreeperModel.java
@@ -10,39 +10,46 @@ import net.minecraft.client.renderer.model.ModelRenderer;
 import net.minecraft.util.math.MathHelper;
 
 public class MiniCreeperModel <T extends MiniCreeperEntity> extends EntityModel<T> {
-	private final ModelRenderer head;
 	private final ModelRenderer body;
+	private final ModelRenderer head;
 	private final ModelRenderer leg1;
 	private final ModelRenderer leg2;
 	private final ModelRenderer leg3;
 	private final ModelRenderer leg4;
-	
 
 	public MiniCreeperModel() {
 		textureWidth = 32;
 		textureHeight = 32;
 
-		
-		this.body = new ModelRenderer(this, 0, 12);
-		this.body.addBox(-3.0F, -10.0F, -1.0F, 6.0F, 7.0F, 3.0F, 0.0F, false);
-		this.body.setRotationPoint(0.0F, 0.0F, 0.0F);
-		this.leg1 = new ModelRenderer(this, 24, 0);
-		this.leg1.addBox(1.0F, -3.0F, -3.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
-		this.leg1.setRotationPoint(-2.0F, 3.0F, 2.0F);
-		this.leg2 = new ModelRenderer(this, 24, 0);
-		this.leg2.addBox(-3.0F, -3.0F, -3.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
-		this.leg2.setRotationPoint(2.0F, 3.0F, 2.0F);
-		this.head = new ModelRenderer(this, 0, 0);
-		this.head.addBox(-3.0F, -16.0F, -3.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
-		this.head.setRotationPoint(0.0F, 10.0F, 0.0F);
-		this.leg3 = new ModelRenderer(this, 24, 0);
-		this.leg3.addBox(1.0F, -3.0F, 2.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
-		this.leg3.setRotationPoint(-2.0F, 3.0F, 1.0F);
-		this.leg4 = new ModelRenderer(this, 24, 0);
-		this.leg4.addBox(-3.0F, -3.0F, 2.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
-		this.leg4.setRotationPoint(2.0F, 3.0F, -1.0F);
-	}
+		body = new ModelRenderer(this);
+		body.setRotationPoint(0.0F, 24.0F, 0.0F);
+		body.setTextureOffset(0, 12).addBox(-3.0F, -10.0F, -1.0F, 6.0F, 7.0F, 3.0F, 0.0F, false);
 
+		head = new ModelRenderer(this);
+		head.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(head);
+		head.setTextureOffset(0, 0).addBox(-3.0F, -16.0F, -3.0F, 6.0F, 6.0F, 6.0F, 0.0F, false);
+
+		leg1 = new ModelRenderer(this);
+		leg1.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(leg1);
+		leg1.setTextureOffset(24, 0).addBox(-3.0F, -3.0F, 2.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
+
+		leg2 = new ModelRenderer(this);
+		leg2.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(leg2);
+		leg2.setTextureOffset(24, 0).addBox(1.0F, -3.0F, 2.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
+
+		leg3 = new ModelRenderer(this);
+		leg3.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(leg3);
+		leg3.setTextureOffset(24, 0).addBox(-3.0F, -3.0F, -3.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
+
+		leg4 = new ModelRenderer(this);
+		leg4.setRotationPoint(0.0F, 0.0F, 0.0F);
+		body.addChild(leg4);
+		leg4.setTextureOffset(24, 0).addBox(1.0F, -3.0F, -3.0F, 2.0F, 3.0F, 2.0F, 0.0F, false);
+	}
 	public Iterable<ModelRenderer> getParts() {
 	      return ImmutableList.of(this.head, this.body, this.leg1, this.leg2, this.leg3, this.leg4);
 	}
@@ -58,14 +65,8 @@ public class MiniCreeperModel <T extends MiniCreeperEntity> extends EntityModel<
 	}
 	
 	@Override
-    public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight,
-                       int packedOverlay, float red, float green, float blue, float alpha) {
-        head.render(matrixStack, buffer, packedLight, packedOverlay);
-        body.render(matrixStack, buffer, packedLight, packedOverlay);
-        leg1.render(matrixStack, buffer, packedLight, packedOverlay);
-        leg2.render(matrixStack, buffer, packedLight, packedOverlay);
-        leg3.render(matrixStack, buffer, packedLight, packedOverlay);
-        leg4.render(matrixStack, buffer, packedLight, packedOverlay);
+	public void render(MatrixStack matrixStack, IVertexBuilder buffer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha){
+		body.render(matrixStack, buffer, packedLight, packedOverlay);
 	}
 	
 	public void setRotationAngle(ModelRenderer modelRenderer, float x, float y, float z) {
