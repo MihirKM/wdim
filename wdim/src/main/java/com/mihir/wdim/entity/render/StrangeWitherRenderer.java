f40b82e7bb81064653ed163fc1b9db639a284fe0
Mihir Mukherjee <mihir.m@digipen.edu>
2021-12-11 18:26:13 -0800

    Sort of fix

--- wdim/src/main/java/com/mihir/wdim/entity/render/StrangeWitherRenderer.java
+++ wdim/src/main/java/com/mihir/wdim/entity/render/StrangeWitherRenderer.java
@@ -1,10 +1,9 @@
 package com.mihir.wdim.entity.render;
 
 import com.mihir.wdim.WdimMod;
-import com.mihir.wdim.entity.custom.MiniCreeperEntity;
 import com.mihir.wdim.entity.custom.StrangeWitherEntity;
-import com.mihir.wdim.entity.model.MiniCreeperModel;
 import com.mihir.wdim.entity.model.StrangeWitherModel;
+import com.mojang.blaze3d.matrix.MatrixStack;
 
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
@@ -16,11 +15,20 @@ public class StrangeWitherRenderer extends MobRenderer<StrangeWitherEntity, Stra
             new ResourceLocation(WdimMod.MOD_ID, "textures/entity/strange_wither.png");
 
     public StrangeWitherRenderer(EntityRendererManager renderManagerIn) {
-        super(renderManagerIn, new StrangeWitherModel<>(), 0.7F);
+        super(renderManagerIn, new StrangeWitherModel<>(), 1.0F);
     }
 
     @Override
     public ResourceLocation getEntityTexture(StrangeWitherEntity entity) {
         return TEXTURE;
     }
+    protected void preRenderCallback(StrangeWitherEntity entitylivingbaseIn, MatrixStack matrixStackIn, float partialTickTime) {
+        float f = 2.0F;
+        int i = entitylivingbaseIn.getInvulTime();
+        if (i > 0) {
+           f -= ((float)i - partialTickTime) / 220.0F * 0.5F;
+        }
+
+        matrixStackIn.scale(f, f, f);
+     }
 }
