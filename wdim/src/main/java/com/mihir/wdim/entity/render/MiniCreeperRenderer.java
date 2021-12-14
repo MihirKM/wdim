d730b5c1af9cd52578f421d829512043b8818d5b
Mihir Mukherjee <mihir.m@digipen.edu>
2021-12-13 17:34:55 -0800

    Creeper optimization

--- wdim/src/main/java/com/mihir/wdim/entity/render/MiniCreeperRenderer.java
+++ wdim/src/main/java/com/mihir/wdim/entity/render/MiniCreeperRenderer.java
@@ -7,7 +7,10 @@ import com.mihir.wdim.entity.model.MiniCreeperModel;
 import net.minecraft.client.renderer.entity.EntityRendererManager;
 import net.minecraft.client.renderer.entity.MobRenderer;
 import net.minecraft.util.ResourceLocation;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 
+@OnlyIn(Dist.CLIENT)
 public class MiniCreeperRenderer extends MobRenderer<MiniCreeperEntity, MiniCreeperModel<MiniCreeperEntity>>
 {
     protected static final ResourceLocation TEXTURE =
